ef5934ca79a1f88cf6b4c5aeccffa1b587936654
dyz <903251743@qq.com>
2022-12-26 02:31:55 +0800

    完成AI初始化

--- backgammon/AI.cpp
+++ backgammon/AI.cpp
@@ -1,1 +1,17 @@
+#include <vector>
+
 #include "AI.h"
+
+void AI::init(Chess* chess)
+{
+	this->chess = chess;
+
+	int size = chess->getGradeSize();
+	for (int i = 0; i < size; i++) {
+		std::vector<int> row;
+		for (int j = 0; j < size; j++) {
+			row.push_back(0);
+		}
+		scoreMap.push_back(row);
+	}
+}
--- backgammon/AI.h
+++ backgammon/AI.h
@@ -1,8 +1,16 @@
+#include <vector>
+
 #include "Chess.h"
 
 class AI
 {
+private:
+	Chess* chess;
+
+	std::vector<std::vector<int>> scoreMap;
+
 public:
+	//≥ı ºªØAI
 	void init(Chess* chess);
 	void go();
 };
